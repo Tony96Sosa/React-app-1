cbe22c7e877ba43273130251846d768959266b33
AnthonySosa <tony96sosa@hotmail.com>
2020-07-19 12:50:31 -0300

    Updates

---
+++ precache-manifest.31b2d104d053bc57f4da7a09e311448a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc43a9988333b611c97df008950921d",
+    "url": "/React-app-1/index.html"
+  },
+  {
+    "revision": "63f70446b5ddd5b7e703",
+    "url": "/React-app-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4203bae7dc20ca46ecd5",
+    "url": "/React-app-1/static/js/2.9dfcfc6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-1/static/js/2.9dfcfc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f70446b5ddd5b7e703",
+    "url": "/React-app-1/static/js/main.18ae2dc1.chunk.js"
+  },
+  {
+    "revision": "4252375774b5ad6b623f",
+    "url": "/React-app-1/static/js/runtime-main.9dae4b66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-app-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
